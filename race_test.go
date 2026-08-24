f92e189887b4ee374f6d1bc59287586192386cef
VirajMandavkar <mandavkarviraj03@gmail.com>
2026-08-24 18:25:08 +0530

    test: add race condition detection for sstCounter

---
+++ race_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestSSTCounterRace(t *testing.T) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			sstCounter++
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			// GET reads the counter with a read-lock
+			mu.RLock()
+			_ = sstCounter
+			mu.RUnlock()
+		}
+	}()
+
+	wg.Wait()
+}
